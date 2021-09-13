566538419f9d8bd11615296aa49622549a85a6c1
jmcfadd <justinmcfadd4@gmail.com>
2021-09-13 07:10:17 -0400

    Exported express app for supertest

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,11 @@ import express, {Request, Response} from 'express'
 import { readdir, readdirSync } from 'fs';
 import mongoose from 'mongoose'
 
-const port = 5000;
+export const port = 5000;
 
-const app = express();
+export const app = express();
 
 readdirSync('./src/routes').map((route: string) => {
   app.use('/api', require(`./routes/${route}`))
 })
 
-app.listen(port, () => console.log(`Listening ${port}`));
---
+++ server/src/server.ts
@@ -0,0 +1,4 @@
+import { app, port } from "./app";
+
+
+app.listen(port, () => console.log(`Listening ${port}`));
