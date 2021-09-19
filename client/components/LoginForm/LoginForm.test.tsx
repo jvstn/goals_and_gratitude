80790113c789ed23e64655f709895dd4a9f0d273
jmcfadd <justinmcfadd4@gmail.com>
2021-09-19 06:44:14 -0400

    Add LoginForm Test Suite

---
+++ client/components/LoginForm/LoginForm.test.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react';
+import { fireEvent, render } from '../../utils/test-utils';
+import LoginForm from '.'
+
+
+describe('LoginForm', () => {
+  it('should render', () => {
+    const { getByText } = render(<LoginForm />)
+
+    expect(getByText('Email Address')).toBeInTheDocument()
+  })
+  it('should have email textbox', () => {
+    const { getByLabelText } = render(<LoginForm />);
+    const emailInput = getByLabelText('email') as HTMLInputElement;
+    fireEvent.change(emailInput, { target: { value: 'test@email.com' } });
+    expect(emailInput.value).toBe('test@email.com');
+  })
+  it('should have password textbox', () => {
+    const { getByLabelText } = render(<LoginForm />);
+    const passwordInput = getByLabelText('password') as HTMLInputElement;
+    fireEvent.change(passwordInput, { target: { value: 'testPass' } });
+    expect(passwordInput.value).toBe('testPass');
+  })
+  it('should have loading text', async () => {
+    const { getByLabelText, getByText } = render(<LoginForm />);
+    const submitButton = getByLabelText('submitButton') as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    const loadingText = await getByText('Submitting');
+    expect(loadingText).toBeTruthy();
+  })
+})
