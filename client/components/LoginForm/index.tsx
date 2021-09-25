bd1a27f26ac84ccac839eaf6b5fecd7096f7affd
jmcfadd <justinmcfadd4@gmail.com>
2021-09-24 20:23:42 -0400

    Added axios base url

--- client/components/LoginForm/index.tsx
+++ client/components/LoginForm/index.tsx
@@ -24,7 +24,7 @@ export default function LoginForm(): ReactElement {
   const handleSubmit = () => {
     setLoading(true);
     axios
-      .post("http://localhost:5000/api/login", { email, password })
+      .post("/api/login", { email, password })
       .then(({ data }) => {
         console.log(data);
         toast({
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import StateProvider from "../context";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.BASE_URL;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
