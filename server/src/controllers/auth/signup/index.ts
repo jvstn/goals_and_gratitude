14efa378fbe62149afca5908c2e2c86a28bf6279
jmcfadd <justinmcfadd4@gmail.com>
2021-09-24 22:40:12 -0400

    Returned user object on sign in

--- server/src/controllers/auth/signup/index.ts
+++ server/src/controllers/auth/signup/index.ts
@@ -8,29 +8,31 @@ dotenv.config();
 export const signupUser = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
-    // Want all fields to be entered
     if (!name) return res.status(400).send("name required");
     if (!password) return res.status(400).send("Password required");
     if (!email) return res.status(400).send("Email required");
 
-    //don't want duplicate accounts
+    //can't have duplicate accounts
     const emailInUse = await User.findOne({ email }).exec();
     if (emailInUse)
       return res.status(400).send("Email already in use. Please login");
 
-    // Don't want plain-text passwords in db
+    // can't have plain-text passwords in db
     const hashedPassword = await hashPassword(password);
-    User.create({ name, email, password: hashedPassword });
-    //In order to protect against XSS and CSRF use cookies
+    const user = await (await User.create({ name, email, password: hashedPassword })).toObject();
+
+    //Use cookies instead of local storage; more secure from XSS
     const token = await jwt.sign({ email }, process.env.JWT_SECRET as string, {
       expiresIn: "14d",
       algorithm: "HS256",
     });
+
+    delete user.password;
     res.cookie("token", token, {
       httpOnly: true,
     });
     res.status(200);
-    res.json({ message: "Successful registration", email, name});
+    res.json({ message: "Successful registration", user});
   } catch (err) {
     console.log(err);
     res.status(500).send("Error please try again later");
