df89bb537ac7f276bbc460a0ef01cf195befedde
jmcfadd <justinmcfadd4@gmail.com>
2021-11-01 01:36:15 -0400

    Update config to mock static files

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,13 @@
-
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   testPathIgnorePatterns: ["./.next/", "./node_modules/"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "identity-obj-proxy",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "./node_modules/babel-jest"
-  }
-}
+    "^.+\\.(js|jsx|ts|tsx)$": "./node_modules/babel-jest",
+  },
+};
