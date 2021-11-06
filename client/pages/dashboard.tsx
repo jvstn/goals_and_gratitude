5c35a8e6012b6a6e38774f3e79fc1dc3521d7d0d
jmcfadd <justinmcfadd4@gmail.com>
2021-11-06 13:08:33 -0400

    Add user name in greeting and date in greeting

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { Box, Container, Flex, Text } from "@chakra-ui/layout";
 import { Center, ListIcon, List } from "@chakra-ui/react";
 import axios from "axios";
+import { format } from "date-fns";
 import { NextPage } from "next";
 import React, { ReactElement, useContext, useEffect } from "react";
 import ItemList from "../components/ItemList";
@@ -9,9 +10,11 @@ import Nav from "../components/Nav";
 import Sidebar from "../components/Sidebar";
 import { Context } from "../context";
 import { IItem } from "../context/itemsReducer";
+import { capitalize } from "../utils/string-utils";
 
 const DashboardPage: NextPage = () => {
   const { state, dispatch } = useContext(Context);
+  const { user } = state;
   useEffect(() => {
     axios
       .get("/api/goals", { params: { date: state.dayToView.toISOString() } })
@@ -36,7 +39,10 @@ const DashboardPage: NextPage = () => {
       <Sidebar>
         <Container centerContent>
           <Text fontSize={"5xl"} fontWeight="bold">
-            Hello, User 👋
+            Hi {user.name ? capitalize(user.name) : "" } 👋 <br />
+          </Text>
+          <Text fontSize={"3xl"}> 
+            Here are your affirmations for {format(state.dayToView, "MMM d")}
           </Text>
           <Container>
             <ItemList type="goals" />
