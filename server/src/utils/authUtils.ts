bf119dd1830a72eacd1f5eed20947ae77eaaeccd
jmcfadd <justinmcfadd4@gmail.com>
2021-11-06 13:05:16 -0400

    Add name property to user model

--- server/src/utils/authUtils.ts
+++ server/src/utils/authUtils.ts
@@ -3,6 +3,7 @@ import { IItem } from "../models/item";
 export interface IUser {
   username: string;
   email: string;
+  name: string;
   password: string;
   createdAt: string;
   goals: IItem[];
